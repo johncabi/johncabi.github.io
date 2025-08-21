cd115a54e3e661d9a8bb3a28f04b3c41e7784ef7
johncabi <johnlloydcabiling14@gmail.com>
2025-08-21 15:18:17 +0800

    chore: update `menu.js` minor fixes.

--- js/menu.js
+++ js/menu.js
@@ -3,34 +3,32 @@ const mobileMenu = document.querySelector("#mobile-menu");
 const mobileMenuOverlay = document.querySelector("#mobile-menu-overlay");
 
 if (window.visualViewport) {
-    let wasOpen = false;
+  let wasOpen = false;
 }
 
 window.visualViewport.addEventListener("resize", () => {
-    const w = window.visualViewport.width;
-    if (w >= 768) {
-        wasOpen = !mobileMenu.classList.contains("-translate-x-full");
-        mobileMenu.classList.add("-translate-x-full");
-        mobileMenuOverlay.classList.add("hidden");
+  const w = window.visualViewport.width;
+  if (w >= 768) {
+    wasOpen = !mobileMenu.classList.contains("-translate-x-full");
+    mobileMenu.classList.add("-translate-x-full");
+    mobileMenuOverlay.classList.add("hidden");
+  } else {
+    if (wasOpen) {
+      mobileMenu.classList.remove("-translate-x-full");
+      mobileMenuOverlay.classList.remove("hidden");
+    } else {
+      mobileMenu.classList.add("-translate-x-full");
+      mobileMenuOverlay.classList.add("hidden");
     }
-    else {
-        if (wasOpen) {
-            mobileMenu.classList.remove("-translate-x-full");
-            mobileMenuOverlay.classList.remove("hidden");
-        }
-        else {
-            mobileMenu.classList.add("-translate-x-full");
-            mobileMenuOverlay.classList.add("hidden");
-        }
-    }
+  }
 });
 
 hamburgerMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("-translate-x-full");
-    mobileMenuOverlay.classList.toggle("hidden")
+  mobileMenu.classList.toggle("-translate-x-full");
+  mobileMenuOverlay.classList.toggle("hidden");
 });
 
 mobileMenuOverlay.addEventListener("click", () => {
-    mobileMenu.classList.add("-translate-x-full");
-    mobileMenuOverlay.classList.add("hidden");
+  mobileMenu.classList.add("-translate-x-full");
+  mobileMenuOverlay.classList.add("hidden");
 });
